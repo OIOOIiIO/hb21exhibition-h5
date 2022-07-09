e80d6ec4025e7f47cbf18bbbf580d1fe725796c0
OIOOIiIO <OIOOIiIO@outlook.com>
2022-07-09 23:52:52 +0800

    Update

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ var page1_text = "<div id='cover-part1'> <div class='cover-indent'><div id='titt
 var page2_text ="<div id='catalog' class='cover-indent'> <div id='catalog-right'> <div class='catalog-block catalog-block-left catalog-right' id='introduction-text'></div> <div class='catalog-block catalog-block-mid catalog-right' id='competition-situation'></div> <div class='catalog-block catalog-block-right catalog-right' id='award'></div> </div> <div id='catalog-left'> <div class='catalog-block catalog-block-left' id='first-prize'></div> <div class='catalog-block catalog-block-mid' id='second-prize'></div> <div class='catalog-block catalog-block-right' id='third-prize'></div> </div> </div> <div id='cover-part1'> <div class='catalog-indent'><div id='catalog-text-img1' class='tittle-part1-left-indent'></div><div id='catalog-text-img2' class='tittle-part1-left-indent'></div></div> </div> <div id='cover-part2'> <div class='back-to-cover-indent next-ico-unity next-ico-border-pre back-to-cover'> <div class='next-ico-arrow-unity next-ico-arrow-pre back-to-cover'></div> </div> <div id='tittle-img' class='catalog-indent'></div> </div> <div id='cover-part3'> <div id='tittle-label-img' class='catalog-indent'></div> </div>"
 var page3_text = "<div id='cover-part1'> <div class='cover-indent'><div id='tittle-bar-img' class='tittle-part1-left-indent'></div></div> <div id='tittle-text-img'><div id='tittle-text-img1' class='tittle-part1-left-indent'></div><div id='tittle-text-img2' class='tittle-part1-left-indent'></div></div> </div> <div id='cover-part2'> <div class='cover-indent next-ico-unity next-ico-border-pre cover-pre'><div class='cover-indent next-ico-arrow-unity next-ico-arrow-pre cover-pre'></div></div> <div id='tittle-img-min' class='cover-indent'></div> <div id='introduction-img'></div> </div> <div id='cover-part3'> <div id='tittle-label-img' class='cover-indent'></div> </div>"
 var chart_page1_text = "<div id='cover-part1'> <div class='cover-indent'><div id='tittle-bar-img' class='tittle-part1-left-indent'></div></div> <div id='tittle-text-img'><div id='tittle-text-img1' class='tittle-part1-left-indent'></div><div id='tittle-text-img2' class='tittle-part1-left-indent'></div></div> </div> <div id='cover-part2'> <div class='cover-indent next-ico-unity next-ico-border-pre cover-pre'> <div class='cover-indent next-ico-arrow-unity next-ico-arrow-pre cover-pre'></div> </div> <div id='tittle-img-min' class='cover-indent'></div> <div class='chart-text' id='cpt-chart-text'></div> <div id='chart-show'> <div id='cpt-chart' class='chart'></div> </div> </div> <div id='cover-part3'> <div id='tittle-label-img' class='cover-indent'></div> </div>"
-var chart_page2_text = "<div id='cover-part1'> <div class='cover-indent'><div id='tittle-bar-img' class='tittle-part1-left-indent'></div></div> <div id='tittle-text-img'><div id='tittle-text-img1' class='tittle-part1-left-indent'></div><div id='tittle-text-img2' class='tittle-part1-left-indent'></div></div> </div> <div id='cover-part2'> <div class='cover-indent next-ico-unity next-ico-border-pre cover-pre'> <div class='cover-indent next-ico-arrow-unity next-ico-arrow-pre cover-pre'></div> </div> <div id='tittle-img-min' class='cover-indent'></div> <div class='chart-text' id='cpt-chart-text'></div> <div id='chart-show'> <div id='awa-chart' class='chart'></div> <div id='map-chart' class='chart'></div> </div> </div> <div id='cover-part3'> <div id='tittle-label-img' class='cover-indent'></div> </div>"
+var chart_page2_text = "<div id='cover-part1'> <div class='cover-indent'><div id='tittle-bar-img' class='tittle-part1-left-indent'></div></div> <div id='tittle-text-img'><div id='tittle-text-img1' class='tittle-part1-left-indent'></div><div id='tittle-text-img2' class='tittle-part1-left-indent'></div></div> </div> <div id='cover-part2'> <div class='cover-indent next-ico-unity next-ico-border-pre cover-pre'> <div class='cover-indent next-ico-arrow-unity next-ico-arrow-pre cover-pre'></div> </div> <div id='tittle-img-min' class='cover-indent'></div> <div class='chart-text' id='awa-chart-text'></div> <div id='chart-show'> <div id='awa-chart' class='chart'></div> <div id='map-chart' class='chart'></div> </div> </div> <div id='cover-part3'> <div id='tittle-label-img' class='cover-indent'></div><div id='slide-ico'></div> </div>"
 var page1_next = function(){
     var next_click = document.getElementsByClassName('cover-next');
     for(var i=0; i<next_click.length; i++)
@@ -67,6 +67,19 @@ var page2_to_awa = function(){
         document.getElementById('container').innerHTML = "";
         document.getElementById('container').innerHTML = chart_page2_text;
         page3_pre();
+        document.getElementById('chart-show').addEventListener('scroll',function(){
+            var scro = document.getElementById('chart-show').scrollTop;
+            var chart_height = document.getElementById('chart-show').clientHeight;
+            if(scro<=chart_height/2)
+            {
+                document.getElementsByClassName('chart-text')[0].setAttribute('id','awa-chart-text')
+            }
+            else if(scro>chart_height/2)
+            {
+                document.getElementsByClassName('chart-text')[0].setAttribute('id','terr-chart-text')
+            }
+        })
+        
     },false)
 }
 
