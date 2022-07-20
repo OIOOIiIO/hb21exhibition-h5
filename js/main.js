5a9a0c50cbbd4313bb16cd38534021e5070f5d7b
OIOOIiIO <OIOOIiIO@outlook.com>
2022-07-20 16:33:54 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -35,3 +35,42 @@ document.documentElement.addEventListener('touchstart', function (event) {
         event.preventDefault();
     }
 }, false);
+
+//预加载图片
+function preloadimages(arr){
+    var newimages=[]
+    var arr=(typeof arr!="object")? [arr] : arr  //确保参数总是数组
+    for (var i=0; i<arr.length; i++){
+        newimages[i]=new Image()
+        newimages[i].src=arr[i]
+    }
+}
+preloadimages(['../src/img/background.png',
+'../src/img/1st-text.png',
+'../src/img/2nd-text.png',
+'../src/img/3rd-text.png',
+'../src/img/1st-text-white.png',
+'../src/img/2nd-text-white.png',
+'../src/img/3rd-text-white.png',
+'../src/img/awa-chart.png',
+'../src/img/awa-chart-text.png',
+'../src/img/cpt-chart.png',
+'../src/img/cpt-chart-text.png',
+'../src/img/map-chart.png',
+'../src/img/terr-chart-text.png',
+'../src/img/awa-text.png',
+'../src/img/cpt-text.png',
+'../src/img/intro-text.png',
+'../src/img/awa-text-white.png',
+'../src/img/cpt-text-white.png',
+'../src/img/intro-text-white.png',
+'../src/img/introduction.png',
+'../src/img/enter.png',
+'../src/img/tittle.png',
+'../src/img/label.png',
+'../src/img/slide.png',
+'../src/img/zbdw.png',
+'../src/img/zddw.png',])
+window.onload = function(){
+    document.getElementById('load').setAttribute('id','load-end');
+}
